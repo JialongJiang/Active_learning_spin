ea89da3db7c7d213059ef1b1b8c62fb9ca033e9a
JialongJiang <jiangjl@caltech.edu>
2019-10-23 15:02:43 -0700

    Update code

--- Code/util_fun/devi_distance.m
+++
@@ -1,9 +0,0 @@
-function [dist] = devi_distance(net1, net2, h_vec)
-%Compute the large deviation distance between two networks
-%   Detailed explanation goes here
-netm = (net1 + net2) / 2;
-dist = - log(para_parti(netm, h_vec))...
-       + (log(para_parti(net1, h_vec)) + log(para_parti(net2, h_vec))) / 2;
-
-end
-
